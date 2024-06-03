842e176867a7b5e13d1706096389128f506a5430
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-03 13:27:32 +0900

    Build frontend.

--- ReactSampler/WebView/vite-project/src/App.tsx
+++ ReactSampler/WebView/vite-project/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// @ts-ignore
 import * as Juce from "juce-framework-frontend";
 
 function App() {
@@ -22,8 +23,9 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => {
+            // @ts-ignore
             sayHelloFromWebView("JUCE")
-            .then((result) => {
+            .then((result: any) => {
               setCount((count) => count + 1);
               console.log(result);
             });
