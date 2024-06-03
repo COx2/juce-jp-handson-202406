bf59ea9d6aa6ae769deecd4af46ab3a382f3eaeb
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-03 09:55:05 +0900

    Fix webview bundle script.

--- ReactSampler/WebView/build-webview-bundle.ts
+++ ReactSampler/WebView/build-webview-bundle.ts
@@ -6,8 +6,9 @@ Please refer to the official website for instructions on how to install Deno.
 https://deno.com/
 */
 
-import {$, CommandBuilder} from "https://deno.land/x/dax/mod.ts";
+import { $ } from "https://deno.land/x/dax/mod.ts";
 import { fromFileUrl } from "https://deno.land/std/path/mod.ts";
+import { exists } from "https://deno.land/std/fs/mod.ts"
 
 //============================================
 console.log("Deno.cwd(): " + Deno.cwd());
@@ -24,4 +25,19 @@ await $`npm run build`;
 
 Deno.chdir(__file_directory + "vite-project/dist");
 
+try {
+    await Deno.remove("../../WebViewBundle.zip");
+} catch (err) {
+    if (!(err instanceof Deno.errors.NotFound)) {
+        console.error(err);
+    }
+}
+
 await $`deno run --allow-write --allow-read --allow-net ${__root_directory}/Tools/deno/mc-zip.ts ../../WebViewBundle.zip ./`
+
+if(!await exists("../../WebViewBundle.zip")){
+    console.error("WebViewBundle.zip creation is failed.");
+    Deno.exit(1)
+}
+
+console.log("WebViewBundle.zip creation is success.");
