69d607924ca1fcd1c5763ade0c41c015095135a7
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-04 16:25:31 +0900

    Add r3f.

--- ReactInstrument/WebView/vite-project/src/App.tsx
+++ ReactInstrument/WebView/vite-project/src/App.tsx
@@ -4,6 +4,7 @@ import MidiKeyboard from './components/MidiKeyboard.tsx'
 import GainKnob from './components/GainKnob.tsx'
 import SoundSelector from './components/SoundSelector.tsx';
 import LoadSampleButton from './components/LoadSampleButton.tsx';
+import { R3FScene } from './components/r3f/Scene.tsx';
 // @ts-ignore
 import * as Juce from "juce-framework-frontend";
 import './App.css'
@@ -39,35 +40,40 @@ function App() {
       );
     };
   }, [setCustomSoundName]);
-
+  
   return (
     <>
-      <div className="control-panel">
-        <div className="control-block">
-          <div className="control-label">
-            SOUND
+      <div className="control-box">
+        <div className="control-panel">
+          <div className="control-block">
+            <div className="control-label">
+              SOUND
+            </div>
+            <div className="control-sound">
+              <SoundSelector/>
+            </div>
+            <div className="control-sound">
+              <LoadSampleButton text="Load Custom Sound"/>
+            </div>
+            <div style={{width: '300px'}}>
+              Custom: {customSoundName}
+            </div>
           </div>
-          <div className="control-sound">
-            <SoundSelector/>
-          </div>
-          <div className="control-sound">
-            <LoadSampleButton text="Load Custom Sound"/>
-          </div>
-          <div style={{width: '300px'}}>
-            Custom: {customSoundName}
+          <div className="control-block">
+            <div className="control-label">
+              GAIN
+            </div>
+            <div className="force-centering">
+              <GainKnob/>
+            </div>
           </div>
         </div>
-        <div className="control-block">
-          <div className="control-label">
-            GAIN
-          </div>
-          <div className="force-centering">
-            <GainKnob/>
-          </div>
+        <div>
+          <MidiKeyboard />
         </div>
       </div>
-      <div>
-        <MidiKeyboard/>
+      <div className="r3f-box">
+        <R3FScene/>
       </div>
     </>
   )
---
+++ ReactInstrument/WebView/vite-project/src/components/r3f/Scene.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei'
+import { Color } from 'three';
+import { Cube } from './Cube';
+import { Sphere } from './Sphere';
+import { Cone } from './Cone';
+import { Torus } from './Torus'
+import { Perf } from 'r3f-perf'
+
+export const R3FScene: FC = () => {
+  const bgColor = new Color("darkgray");
+
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 1000,
+        position: [0, 0, 5]
+      }}
+    >
+      {/* <Perf/> */}
+      <color
+        attach="background"
+        args={[bgColor.r, bgColor.g, bgColor.b]}
+      />
+      <Cube position={[-2.2, 1.2, -1.0]}/>
+      <Sphere position={[2.2, 1.2, -1.0]}/>
+      <Cone position={[-2.2, -1.2, -1.0]}/>
+      <Torus position={[2.2, -1.2, -1.0]}/>
+      <OrbitControls />
+      <ambientLight args={[0xffffff]} intensity={0.2} />
+      <directionalLight position={[0.3, 0.4, 1]} intensity={0.8} />
+    </Canvas>
+  );
+};
