a58fc73eb75720cc1f4109cc3ef9847053996a35
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-04 17:07:52 +0900

    Change aspect ratio.

--- ReactInstrument/Source/PluginEditor.cpp
+++ ReactInstrument/Source/PluginEditor.cpp
@@ -35,7 +35,7 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
 
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (1440, 960);
+    setSize (1440, 460);
     setResizable(true, true);
 
     processorRef.getCustomSoundFileValue().addListener(this);
