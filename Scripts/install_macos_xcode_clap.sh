1a71ad14bae58ee7161646bbf0be45ed2af8736a
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-05 22:33:45 +0900

    Add install script for macOS.

---
+++ Scripts/install_macos_xcode_clap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e -u -o pipefail
+
+realpath() {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+SCRIPT_NAME="$(basename "$(realpath "$0")")"
+SCRIPT_DIRECTORY="$(dirname "$(realpath "$0")")"
+
+cd "${SCRIPT_DIRECTORY}/../"
+
+echo "Install Release version..."
+cmake --install "${SCRIPT_DIRECTORY}/../builds/xcode" --prefix Release --component ReactInstrument_CLAP
