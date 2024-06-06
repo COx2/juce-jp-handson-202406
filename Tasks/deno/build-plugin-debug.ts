15acd1fccaff85f813699344f43000990b5c945d
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-06-06 15:38:52 +0900

    Add tasks debug version.

---
+++ Tasks/deno/build-plugin-debug.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S deno run --allow-all
+
+/*
+This script requires the Deno runtime environment to execute.
+Please refer to the official website for instructions on how to install Deno.
+https://deno.com/
+*/
+
+import { $ } from "https://deno.land/x/dax/mod.ts";
+import { fromFileUrl } from "https://deno.land/std/path/mod.ts";
+
+//============================================
+console.log("Deno.cwd(): " + Deno.cwd());
+
+const __root_directory = fromFileUrl(new URL("../../", import.meta.url));
+Deno.chdir(__root_directory);
+
+console.log("Deno.cwd(): " + Deno.cwd());
+
+const platform = Deno.build.os;
+
+console.log("Deno.build.os: " + platform.toString())
+
+if(platform.toString() === "windows")
+{
+    await $`cmake --preset vs2022`;
+    await $`cmake --build --preset vs2022-debug`;
+}
+else if(platform.toString() === "darwin")
+{
+    await $`cmake --preset xcode`;
+    await $`cmake --build --preset xcode-debug`;
+}
+else if(platform.toString() === "linux")
+{
+    await $`cmake --preset ninja-single-debug`;
+    await $`cmake --build --preset ninja-single-debug`;
+}
